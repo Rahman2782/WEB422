b86dd4b713e9ac7d69a2d210f34a54ef4a75a16d
Rahman Ahmed <rahman2782@gmail.com>
2025-08-09 05:10:08 -0400

    updated library-mgmt-sys folder to AS1, adding midterm questions for easier writing and reference

---
+++ Midterm-Qs/Q2/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+})
